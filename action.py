a85eccd31b8e70b88cb9d3b8cb214dcf734dd9b1
jason <jason@zzq.org>
2025-01-14 14:47:52 -0700

    fix api url again

--- action.py
+++ action.py
@@ -11,10 +11,9 @@ import urllib.request
 
 GH_TOKEN = os.getenv("GITHUB_TOKEN", None)
 GH_API_URL_BASE = os.getenv("GITHUB_API_URL", None)
-GH_OWNER = os.getenv("GITHUB_REPOSITORY_OWNER", None)
-GH_REPO_NAME = os.getenv("GITHUB_REPOSITORY", None)
+GH_REPO_PATH = os.getenv("GITHUB_REPOSITORY", None)
 
-GH_API_URL = f"{GH_API_URL_BASE}/{GH_OWNER}/{GH_REPO_NAME}"
+GH_API_URL = f"{GH_API_URL_BASE}/{GH_REPO_PATH}"
 
 class HttpError(Exception):
     """Describe an unrecoverable HTTP error."""
